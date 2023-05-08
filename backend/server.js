10d3aee84907a53371551b643937166996ef7345
Fjallroth <christer.fjallroth@gmail.com>
2023-05-08 10:36:58 +0200

    vercel

--- backend/server.js
+++ backend/server.js
@@ -30,6 +30,10 @@ app.use(
 // }));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "../client/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+});
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(logger("dev"));
