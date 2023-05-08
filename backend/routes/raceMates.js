f520978f66709baeeeeafa00e18f348811dd038d
Fjallroth <christer.fjallroth@gmail.com>
2023-05-08 19:41:23 +0200

    auth issues

--- backend/routes/raceMates.js
+++ backend/routes/raceMates.js
@@ -5,23 +5,27 @@ const homeController = require("../controllers/home");
 
 const { ensureAuth } = require("../middleware/auth");
 
-router.get("/", homeController.getReact);
+router.get("/", ensureAuth, homeController.getReact);
 
-router.get("/races", raceController.getRaces);
+router.get("/races", ensureAuth, raceController.getRaces);
 
-router.post("/planRace", raceController.planRace); // add ensure Auth
+router.post("/planRace", ensureAuth, raceController.planRace); // add ensure Auth
 
-router.put("/joinRace", raceController.joinRace);
+router.put("/joinRace", ensureAuth, raceController.joinRace);
 
-router.put("/approveJoin/:raceId/:userId", raceController.approveJoin);
+router.put(
+  "/approveJoin/:raceId/:userId",
+  ensureAuth,
+  raceController.approveJoin
+);
 
-router.post("/selectRide", raceController.selectRide);
+router.post("/selectRide", ensureAuth, raceController.selectRide);
 
-router.put("/submitRide", raceController.submitRide);
+router.put("/submitRide", ensureAuth, raceController.submitRide);
 
-router.get("/linkStrava", raceController.linkStrava);
+router.get("/linkStrava", ensureAuth, raceController.linkStrava);
 
-router.get("/stravaCallback", raceController.stravaCallback);
+router.get("/stravaCallback", ensureAuth, raceController.stravaCallback);
 
 //delete ride?
 
