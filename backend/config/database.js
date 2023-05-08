df0b1550b7450439af858f6621c0d40c7945ed5d
Fjallroth <christer.fjallroth@gmail.com>
2023-05-08 10:57:43 +0200

    vercel

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,19 +1,23 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
+
+require("dotenv").config({ path: "./config/.env" });
 
 const connectDB = async () => {
   try {
+    console.log("MONGODB_URI:", process.env.DB_STRING); // Add this line to print the value
+
     const conn = await mongoose.connect(process.env.DB_STRING, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useFindAndModify: false,
-      useCreateIndex: true
-    })
+      useCreateIndex: true,
+    });
 
-    console.log(`MongoDB Connected: ${conn.connection.host}`)
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
-    console.error(err)
-    process.exit(1)
+    console.error("Error connecting to MongoDB:", err);
+    process.exit(1);
   }
-}
+};
 
-module.exports = connectDB
+module.exports = connectDB;
