1e4320c2292362e8b683b2402e715c944ce81adc
Fjallroth <christer.fjallroth@gmail.com>
2023-05-08 13:43:37 +0200

    db problems

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,13 +1,13 @@
 module.exports = {
-    ensureAuth: function (req, res, next) {
-      if (req.isAuthenticated()) {
-        console.log(req.sessionID)
-        return next()
-      } else {
-        console.log(req.sessionID)
-        console.log("user not authenicated")
-        return res.redirect('/')
-      }
+  ensureAuth: function (req, res, next) {
+    if (req.isAuthenticated()) {
+      console.log(req.sessionID);
+      console.log("ensureAuth middleware called");
+      return next();
+    } else {
+      console.log(req.sessionID);
+      console.log("user not authenicated");
+      return res.redirect("/");
     }
-  }
-  
+  },
+};
