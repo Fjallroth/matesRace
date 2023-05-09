b607b67ba75b74f5c22ab193e90b6c948f90c121
Fjallroth <christer.fjallroth@gmail.com>
2023-05-09 13:00:35 +0200

    jwt experiment

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -26,36 +26,35 @@ exports.postLogin = (req, res, next) => {
     gmail_remove_dots: false,
   });
 
-  passport.authenticate("local", { session: false }, (err, user, info) => {
+  User.findOne({ email: req.body.email }, (err, user) => {
     if (err) {
       return next(err);
     }
     if (!user) {
       return res.status(400).json({
         message: "Something is not right",
-        user: user,
       });
     }
-    req.login(user, { session: false }, (err) => {
+    user.comparePassword(req.body.password, (err, isMatch) => {
       if (err) {
         return next(err);
       }
-      const token = jwt.sign(user.toJSON(), process.env.JWTKey); //createkeys
+      if (!isMatch) {
+        return res.status(400).json({
+          message: "Something is not right",
+        });
+      }
+      const token = jwt.sign(user.toJSON(), process.env.JWTKey, {
+        expiresIn: "8h",
+      }); //createkeys
       return res.json({ user, token });
     });
-  })(req, res, next);
+  });
 };
 
 exports.logout = (req, res) => {
-  req.logout(() => {
-    console.log("User has logged out.");
-  });
-  req.session.destroy((err) => {
-    if (err)
-      console.log("Error : Failed to destroy the session during logout.", err);
-    req.user = null;
-    res.redirect("/");
-  });
+  res.clearCookie("token");
+  res.redirect("/");
 };
 
 exports.getSignup = (req, res) => {
@@ -105,7 +104,6 @@ exports.postSignup = (req, res, next) => {
           return next(err);
         }
         const token = jwt.sign({ id: user._id }, process.env.JWTKey, {
-          //createkeys
           expiresIn: "8h",
         });
         return res.json({ user, token });
