8c8d2aaadf122c664eb8e56fc2725f5ec8e37d6e
Fjallroth <christer.fjallroth@gmail.com>
2025-04-27 16:15:10 +0200

    added server.js

---
+++ backend/server.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const app = express();
+const passport = require("passport");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+const mongoose = require("mongoose");
+const connectDB = require("./config/database");
+const mainRoutes = require("./routes/main");
+const raceRoutes = require("./routes/raceMates");
+let cors = require("cors");
+const path = require("path");
+require("dotenv").config({ path: "./config/.env" });
+
+require("./config/passport")(passport);
+
+connectDB();
+
+app.set("views", path.join(__dirname, "../client/build"));
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "../client/build")));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(logger("dev"));
+app.use(cookieParser());
+app.use(passport.initialize());
+
+app.use(
+  session({
+    secret: process.env.JWTKey,
+    cookie: { maxAge: 60000 * 60 * 8, sameSite: "lax" },
+    resave: true,
+    saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  })
+);
+
+app.use(
+  cors({
+    origin:
+      process.env.NODE_ENV === "production"
+        ? "https://mates-race.vercel.app/"
+        : "http://localhost:2121",
+  })
+);
+app.use("/", mainRoutes);
+app.use("/raceMates", raceRoutes);
+app.listen(process.env.PORT, () => {
+  console.log("Server is running, you better catch it!");
+});
+module.exports = app;
